efb66ce76bd51f176e8ab2879a28dcc1d8d565c2
sarah <smile-wu@hotmail.com>
2017-07-22 17:37:17 +0800

    implement post edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,13 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,8 +22,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.delete
+    redirect_to account_posts_path, alert: "Post Deleted!"
+
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission!"
+    end
   end
 end
